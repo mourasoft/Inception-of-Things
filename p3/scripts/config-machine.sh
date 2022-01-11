6e324b8c6ff13db4a353f51722c8c7f26b42e7d9
mourasoft <mourad.meski@gmail.com>
2022-01-11 17:11:39 +0100

    start ex 3

---
+++ p3/scripts/config-machine.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get update 
+
+# install docke
+apt-get install -y docker.io
+
+
+# #install k3d
+# curl -s https://raw.githubusercontent.com/rancher/k3d/master/install.sh | bash
+
+# #install google apt repo
+#  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+#  #add google apt repo
+
